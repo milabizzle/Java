40c721c486736e21032ba17c048b302dad75bdcc
milabizzle <parcerias.cr@gmail.com>
2023-05-29 15:43:41 -0300

    Laços de Repetição

--- src/LacosDeRepeticao/For.java
+++ src/LacosDeRepeticao/For.java
@@ -1,9 +1,36 @@
 package LacosDeRepeticao;
+//Exercicio 2
+
+import java.util.Scanner;
 
 public class For {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner leia = new Scanner(System.in);
+		
+		int contador = 0, numero, pares = 0, impares = 0;
+
+		for(contador = 1; contador <= 10; contador++){
+			System.out.println("\nDigite o " + contador + "º número: ");
+			numero = leia.nextInt();{
+			
+			}
+		
+		if (numero %2 == 0) {
+			pares++;
+		}
+		else {
+			impares++;
+			System.out.println("Total de números pares: " + pares);
+			System.out.println("Total de números impares: " + impares);
+			
+			//Descobrir como deixar o print somente no encerramento do contador.
+			
+		}
+		
+		
+		}
 
 	}
 
