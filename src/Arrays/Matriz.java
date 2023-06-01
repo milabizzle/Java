49247e0ebc0966a3e6e063158dc87ebdcc073212
milabizzle <parcerias.cr@gmail.com>
2023-06-01 14:20:57 -0300

    Arrays

--- src/Arrays/Matriz.java
+++ src/Arrays/Matriz.java
@@ -5,11 +5,26 @@ public class Matriz {
 	public static void main(String[] args) {
 		
 		int matrizNum [][] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+		
+		int somaPrincipal = 0, somaSecundaria = 0;
+		String principal = "", secundaria = "";
+		
+		for (int indice = 0; indice < matrizNum.length; indice++) {
+			somaPrincipal += matrizNum[indice][indice];
+			
+			principal += matrizNum[indice][indice] + " " ;
+		}
 
-		System.out.println("Elementos da diagonal principal: " + matrizNum[0][0] + " " + matrizNum[1][1] + " " + matrizNum[2][2]);
-		System.out.println("\nElementos da diagonal secundária: " + matrizNum[0][2] + " " + matrizNum[1][1] + " " + matrizNum[2][0]);
-		System.out.println("\nSoma do elementos da diagonal principal: " + (matrizNum[0][0] + matrizNum[1][1] + matrizNum[2][2]));
-		System.out.println("\nSoma do elementos da diagonal secundária: " + (matrizNum[0][2] + matrizNum[1][1] + matrizNum[2][0]));
+		for (int indice = 0; indice < matrizNum.length; indice++) {
+			somaSecundaria += matrizNum[indice][matrizNum.length -1 - indice];
+			
+			secundaria += matrizNum[indice][matrizNum.length -1 - indice] + " " ;
+		}
+
+		System.out.println("Elementos da diagonal principal: " + principal);
+		System.out.println("\nElementos da diagonal secundária: " + secundaria);
+		System.out.println("\nSoma do elementos da diagonal principal: " + somaPrincipal);
+		System.out.println("\nSoma do elementos da diagonal secundária: " + somaSecundaria);
 	         
 	}
 }
--- src/EstruturaDeDados/Pilha.java
+++ src/EstruturaDeDados/Pilha.java
@@ -1,10 +1,28 @@
 package EstruturaDeDados;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Pilha {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Stack<String> pilha = new Stack<String>();
+		
+		Scanner leia = new Scanner(System.in);
+		
+		String livro;
+		int opcao;
+		
+		System.out.println("Escolha a opção desejada: ");
+		opcao = leia.nextInt();
+		
+		
+			System.out.println("Digite o nome: ");
+			livro = leia.nextLine();
+			pilha.push(livro);
+		
+		}
 	}
 
-}
+
