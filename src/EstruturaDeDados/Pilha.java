4bef7382d111460429bd011b6eaf8d6f705f7be9
milabizzle <parcerias.cr@gmail.com>
2023-06-02 14:44:39 -0300

    EstruturaDeDados

--- src/EstruturaDeDados/Pilha.java
+++ src/EstruturaDeDados/Pilha.java
@@ -2,27 +2,63 @@ package EstruturaDeDados;
 
 import java.util.Scanner;
 import java.util.Stack;
+import java.util.Iterator;
 
 public class Pilha {
 
 	public static void main(String[] args) {
-		
+
 		Stack<String> pilha = new Stack<String>();
-		
 		Scanner leia = new Scanner(System.in);
-		
-		String livro;
-		int opcao;
-		
-		System.out.println("Escolha a opção desejada: ");
-		opcao = leia.nextInt();
-		
-		
-			System.out.println("Digite o nome: ");
-			livro = leia.nextLine();
-			pilha.push(livro);
-		
+
+		String livro = null;
+		int opcao = 0;
+		Boolean faca = true;
+
+		while (faca) {
+			System.out.println("\nEscolha a opção desejada:" + "\n1 - Adicionar livro na pilha"
+					+ "\n2 - Listar todos os livros" + "\n3 - Retirar livro da pilha" + "\n0 - Sair");
+			opcao = leia.nextInt();
+
+			switch (opcao) {
+
+			case 1:
+				System.out.println("Digite o nome do livro: ");
+				leia.skip("\\R?");
+				livro = leia.nextLine();
+				pilha.push(livro);
+				System.out.println("\nLivro Adicionado!!\n");
+
+				break;
+
+			case 2:
+
+				if (pilha.size() == 0) {
+					System.out.println("A Pilha está vazia!!");
+				} else {
+					System.out.println("\nLivros da Pilha: " + pilha);
+				}
+				break;
+
+			case 3:
+				if (pilha.size() == 0) {
+					System.out.println("A pilha está vazia.\n");
+				} else {
+					System.out.println("\nLivros na Pilha: " + pilha.pop());
+					System.out.println("Um livro foi retirado da pilha\n");
+				}
+				break;
+
+			default:
+				if (opcao == 0) {
+					faca = false;
+					System.out.println("Programa finalizado!");
+				} else {
+					System.out.println("Opção inválida!!");
+				}
+				break;
+
+			}
 		}
 	}
-
-
+}
--- src/EstruturaDeDados/QueueFila.java
+++ src/EstruturaDeDados/QueueFila.java
@@ -3,8 +3,9 @@ package EstruturaDeDados;
 public class QueueFila {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
+		
 	}
 
 }
